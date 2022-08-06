0a7fd1139aa7e5453593031201decd4f6069f01e
Oscar Rivera <24orivera@woodward.edu>
2022-08-06 13:33:06 -0400

    d

--- app.py
+++ app.py
@@ -7,20 +7,22 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import psycopg2
 from flask_mail import Mail, Message
 
+# Connecting my PostgresSQL table so users can register and login
 DATABASE_URL = "postgres://kdwwsivrbhavjf:d3e7832a1ba7ee6055e00c9300f913227bdae6f398316babbf53f19ad2590486@ec2-44-208-88-195.compute-1.amazonaws.com:5432/d86u2pacceck0q"
 
+# Connection and cursor to allow me to run SQL queries in my app.py
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
 cur = conn.cursor()
 
 
 app = Flask(__name__)
 
-
+# Configure session to use filesystem (instead of signed cookies) - From CS50 Finance
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# CS50 Finance login required function 
 
 def login_required(f):
     """
@@ -34,7 +36,7 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-
+# Ensure responses aren't cached - From CS50 Finance
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -43,13 +45,16 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
+# Login page
 @app.route("/login.html", methods=["GET", "POST"])
 def login():
+    # Renders page get requests
     if request.method == "GET":
         return render_template("login.html", invalid=0)
+    # Checks login information and logs user in
     if request.method == "POST":
         if not request.form.get("uname"):
+            # Pass an argument that causes an alert if the user uses incorrect login information
             return render_template("login.html", invalid=1)
         if not request.form.get("psw"):
             return render_template("login.html", invalid=2)
@@ -65,41 +70,44 @@ def login():
         session["user_id"] = user_id[0]
         return redirect("/")
 
-
+# Redirects to main page
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     return render_template("thepark.html")
 
-
+# Page made for when my app scales up -- I will make more pages that include service opportunities in specific areas 
 @app.route("/midtown.html", methods=["GET", "POST"])
 @login_required
 def midtown():
     return render_template("midtown.html")
 
-
+# Page made for when my app scales up -- I will make more pages that include service opportunities in specific areas 
 @app.route("/buckhead.html", methods=["GET", "POST"])
 @login_required
 def buckhead():
     return render_template("buckhead.html")
 
-
+# Working page that displays available service opportunities
 @app.route("/thepark.html", methods=["GET", "POST"])
 @login_required
 def thepark():
     return render_template("thepark.html")
 
-
+# Register page 
 @app.route("/register.html", methods=["GET", "POST"])
 def register():
+    # Renders page upon get request
     if request.method == "GET":
         return render_template("register.html", invalid=0)
+    # Inserts user into database upon post request
     if request.method == "POST":
         username = request.form.get("user")
         pass_one = request.form.get("psw")
         pass_two = request.form.get("rptpsw")
         cur.execute("SELECT username FROM login WHERE username = %s", (username,))
         usernames = cur.fetchone()
+        # Makes sure passwords match and user doesn't already have an account
         if usernames is not None:
             invalid = 1
             return render_template("register.html", invalid=invalid)
